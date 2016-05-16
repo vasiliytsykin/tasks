6cc290f5c38c40dbffef0b55587846a82dde43a6
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-05-16 20:59:29 +0500

    Simql 2 done!

--- SimQL/SimQLExecutor.cs
+++ SimQL/SimQLExecutor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -15,21 +16,30 @@ namespace SimQLTask
             this.data = data;
         }
 
-
         public string ExecuteQuery(string query)
         {
             Match m;
 
-            if (Match(@"sum\((?<path>.*)\)", query, out m))
-                return $"{query} = {data.GetSequence(m.Groups["path"].Value).Sum()}";
-            if (Match(@"min\((?<path>.*)\)", query, out m))
-                return $"{query} = {data.GetSequence(m.Groups["path"].Value).Min()}";
-            if (Match(@"max\((?<path>.*)\)", query, out m))
-                return $"{query} = {data.GetSequence(m.Groups["path"].Value).Max()}";
+            if (Match(@"sum\((?<path>[\w.]+)\s?(from\s(?<from>\d+))?\s?(top\s(?<top>\d+))?\)", query, out m))
+                return $"{query} = { FilterSequence(data.GetSequence(m.Groups["path"].Value), m).Sum() }";
+            if (Match(@"min\((?<path>[\w.]+)\s?(from\s(?<from>\d+))?\s?(top\s(?<top>\d+))?\)", query, out m))
+                return $"{query} = { FilterSequence(data.GetSequence(m.Groups["path"].Value), m).Min() }";
+            if (Match(@"max\((?<path>[\w.]+)\s?(from\s(?<from>\d+))?\s?(top\s(?<top>\d+))?\)", query, out m))
+                return $"{query} = { FilterSequence(data.GetSequence(m.Groups["path"].Value), m).Max() }";
             throw new ArgumentException("incorrect query");
         }
 
-
+        private IEnumerable<double> FilterSequence(IEnumerable<double> seq, Match m)
+        {
+            var from = m.Groups["from"];
+            var top = m.Groups["top"];
+            if (from.Length != 0)
+                seq = seq.Where(x => x >= double.Parse(from.Value));
+            if (top.Length != 0)
+                seq = seq.Take(int.Parse(top.Value));
+            
+            return seq;
+        }
      
 
         private bool Match(string pattern, string command, out Match m)
--- SimQL/SimQLProgram_Should.cs
+++ SimQL/SimQLProgram_Should.cs
@@ -15,7 +15,35 @@ namespace SimQLTask
         }
 
         [Test]
-        public void ComplexSum()
+        public void ComplexSumQuery()
+        {
+            var results = SimQLProgram.ExecuteQueries(@"{'data': {'z':[1,2,3,4,5,6,7,8,9,10,11,12]},
+                                                        'queries': [ 'sum(z from 3 top 4)']}");
+
+            Assert.AreEqual(new[] { "sum(z from 3 top 4) = 18" }, results);
+        }
+
+        [Test]
+        public void ComplexSumQueryFromOnly()
+        {
+            var results = SimQLProgram.ExecuteQueries(@"{'data': {'z':[1,2,3,4,5,6,7,8,9,10,11,12]},
+                                                        'queries': [ 'sum(z from 11)']}");
+
+            Assert.AreEqual(new[] { "sum(z from 11) = 23" }, results);
+        }
+
+
+        [Test]
+        public void ComplexSumQueryTopOnly()
+        {
+            var results = SimQLProgram.ExecuteQueries(@"{'data': {'z':[1,2,3,4,5,6,7,8,9,10,11,12]},
+                                                        'queries': [ 'sum(z top 5)']}");
+
+            Assert.AreEqual(new[] { "sum(z top 5) = 15" }, results);
+        }
+
+        [Test]
+        public void ComplexQuery()
         {
             var results = SimQLProgram.ExecuteQueries(@"{'data': {'a':[{'b': [3.14, 4]}, {'b':[{'c':15}, {'c':9}]}], 'z':[2.65, 35]},
                                                         'queries': [ 'sum(a.b.c)']}");
