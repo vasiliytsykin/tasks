01fab7d9ebfa28004fdb88f2359a2e8223f922e4
vasiliytsykin <vasiliytsykin@gmail.com>
2016-05-18 23:00:34 +0500

    parser changed

--- Eval/Evaluator.cs
+++ Eval/Evaluator.cs
@@ -11,11 +11,24 @@ namespace EvalTask
         High
     }
 
+    enum Lexem
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Sqrt,
+        Percent,
+        OpBracket,
+        ClBracket
+    }
+
     public class Evaluator
     {
 
         private readonly Dictionary<string, Func<double, double, double>> operations;
         private readonly Dictionary<string, Priority> priorities;
+        private readonly string[] lexems = {"sqrt", "+", "-", "*", "/", "%", "(", ")"};
 
         public Evaluator()
         {
@@ -32,7 +45,9 @@ namespace EvalTask
                 {"+", Priority.Low },
                 {"-", Priority.Low },
                 {"*", Priority.High },
-                {"/", Priority.High }
+                {"/", Priority.High },
+                {"sqrt", Priority.High },
+                {"%", Priority.High }
             };
         }
 
@@ -40,39 +55,10 @@ namespace EvalTask
         {
             var polishNotation = ConvertToPolishNotation(expression);
             return CalculateFromPolishNotation(polishNotation);
-        }
+        }            
 
-        private string ConvertToPolishNotation(string expression)
-        {
-            var outQueue = new StringBuilder();
-            var operators = new Stack<string>();
-
-            foreach (var token in expression)
-            {
-                if(char.IsDigit(token)) outQueue.Append(token + " ");                
-                else if(operations.ContainsKey(token.ToString()))
-                HandleSituation(outQueue, operators, token.ToString());
-            }
-
-            while (operators.Count != 0)
-            {
-                outQueue.Append(operators.Pop() + " ");
-            }
-
-            return outQueue.ToString();
-        }
-
-        private void HandleSituation(StringBuilder outQueue, Stack<string> operators, string token)
-        {
-            if (operators.Count != 0 && priorities[token] <= priorities[operators.Peek()])          
-                outQueue.Append(operators.Pop() + " ");               
-            
-            operators.Push(token);
-        }
-
-        private double CalculateFromPolishNotation(string expression)
-        {
-            var tokens = expression.Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        private double CalculateFromPolishNotation(List<string> tokens)
+        {            
             var operands = new Stack<double>();
 
             foreach (var token in tokens)
@@ -84,6 +70,103 @@ namespace EvalTask
 
             return operands.Pop();
 
+        }
+
+        private List<string> ConvertToPolishNotation(string expression)
+        {
+            var res = new List<string>();
+            var stack = new Stack<string>();
+
+            return ConvertToPolishNotation(expression, res, stack, 0);
+        }
+
+        private List<string> ConvertToPolishNotation(string exp, List<string> res, Stack<string> stack , int startIndex)
+        {
+            if (startIndex == exp.Length)
+            {
+                while (stack.Count != 0)
+                {
+                    res.Add(stack.Pop());
+                }
+                return res;
+            }
+
+            var index = startIndex;
+
+            foreach (var lexem in lexems)
+            {
+                var strRestLength = exp.Substring(startIndex).Length;
+
+                if (lexem.Length <= strRestLength && lexem == exp.Substring(startIndex, lexem.Length))
+                {
+                    if (IsUnaryMinus(stack, lexem) || lexem == "sqrt" || lexem == "%")
+                    {
+                        res.Add("0");
+                        stack.Push(lexem);
+                    }
+
+                    else HandleSituation(res, stack, lexem);
+
+                    startIndex += lexem.Length;
+                }
+            }
+
+            if (startIndex == index)
+                res.Add(ReadNumber(exp, ref startIndex));
+
+            SkipWhiteSpaces(exp, ref startIndex);
+
+            return ConvertToPolishNotation(exp, res, stack, startIndex);
+        }
+
+        private void HandleSituation(List<string> res, Stack<string> stack, string lexem)
+        {
+
+            if (lexem == "(")
+            {
+                stack.Push(lexem);
+                return;
+            }
+
+            if (lexem == ")")
+            {
+                while (stack.Peek() != "(")
+                {
+                    res.Add(stack.Pop());
+                }
+
+                stack.Pop();
+                return;
+            }
+
+            if (stack.Count != 0 && priorities[lexem] <= priorities[stack.Peek()])
+                res.Add(stack.Pop());
+
+            stack.Push(lexem);
+        }
+
+        private void SkipWhiteSpaces(string exp, ref int startIndex)
+        {
+            while (startIndex < exp.Length && char.IsWhiteSpace(exp[startIndex]))        
+                startIndex++;          
+        }
+
+        private string ReadNumber(string exp, ref int startIndex)
+        {
+            StringBuilder str = new StringBuilder();
+
+            while (startIndex < exp.Length && (char.IsDigit(exp[startIndex]) || char.IsPunctuation(exp[startIndex])))
+            {
+                str.Append(exp[startIndex]);
+                startIndex++;
+            }
+
+            return str.ToString();
+        }
+
+        private static bool IsUnaryMinus(Stack<string> stack, string lexem)
+        {
+            return lexem == "-" && (stack.Count == 0 || stack.Peek() == "(");
         }
     }
 }
